3d7d9aeb0c98d654c83d7a23c535e4631d9a8d3e
Nicklas Fulghum <Nicklas.Fulghum@ibm.com>
2019-09-11 16:39:28 -0400

    Plantman front-end

--- vendors/js/script.js
+++ vendors/js/script.js
@@ -11,16 +11,16 @@ $(document).ready(function() {
     },{
         offset: '60px;'
     });
-    
-    /* Scroll on buttons */    
+
+    /* Scroll on buttons */
     $('.js--top-btn').click(function(){
         $('html, body').animate({scrollTop: $('.js--top-of-page').offset().top}, 1000);
     });
-    
+
     $('.js--btn-full').click(function(){
         $('html, body').animate({scrollTop: $('.js--section-form').offset().top}, 1000);
     });
-    
+
     /* Navigation scroll */
     $(function() {
       $('a[href*="#"]:not([href="#"])').click(function() {
@@ -36,32 +36,32 @@ $(document).ready(function() {
         }
       });
     });
-    
-    
-    /* Animations on scroll */    
+
+
+    /* Animations on scroll */
     $('.js--top-of-page').waypoint(function(direction) {
         $('.js--top-of-page').addClass('animated bounce');
     },{
         offset: '50%'
     });
-    
+
     $('.js--wp-1').waypoint(function(direction) {
         $('.js--wp-1').addClass('animated pulse');
     },{
         offset: '50%'
     });
-    
+
     $('.js--wp-2').waypoint(function(direction) {
         $('.js--wp-2').addClass('animated pulse');
     },{
         offset: '50%'
     });
-    
+
     /* Mobile Nav */
     $('.js--nav-icon').click(function() {
         var nav = $('.js--main-nav');
         var icon = $('.js--nav-icon i');
-        
+
         nav.slideToggle(200);
         if (icon.hasClass('ion-navicon-round')){
             icon.addClass('ion-close-round');
@@ -70,6 +70,39 @@ $(document).ready(function() {
             icon.addClass('ion-navicon-round');
             icon.removeClass('ion-close-round');
         }
-        
+
     });
+
+    var textWrapper = document.querySelector('.ml11 .letters');
+    textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+
+    anime.timeline({loop: false})
+      .add({
+        targets: '.ml11 .line',
+        scaleY: [0,1],
+        opacity: [0.5,1],
+        easing: "easeOutExpo",
+        duration: 1000,
+        delay: 500
+      })
+      .add({
+        targets: '.ml11 .line',
+        translateX: [0, document.querySelector('.ml11 .letters').getBoundingClientRect().width],
+        easing: "easeOutExpo",
+        duration: 2000,
+        delay: 0
+      }).add({
+        targets: '.ml11 .letter',
+        opacity: [0,1],
+        easing: "easeOutExpo",
+        duration: 2000,
+        offset: '-=775',
+        delay: (el, i) => 34 * (i+1)
+      }).add({
+        targets: '.ml11 .line',
+        opacity: 0,
+        duration: 2000,
+        easing: "easeOutExpo",
+        delay: 0
+      });
 });
